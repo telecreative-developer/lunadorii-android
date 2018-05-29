82667a19afc12c81074538fe5c4ad1b83fa1bfd5
Hyperspace018 <hyperspace018@gmail.com>
2018-05-29 05:18:02 -0400

    Create Edit Address Screen

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ import HomeContainer from './app/containers/HomeContainer'
 import ProfileContainer from './app/containers/ProfileContainer'
 import PaymentsContainer from './app/containers/PaymentsContainer'
 import ChangePasswordContainer from './app/containers/ChangePasswordContainer'
+import EditAddressContainer from './app/containers/EditAddressContainer'
 
 const App = StackNavigator(
   { 
+    EditAddressContainer: { screen: EditAddressContainer },
     HomeContainer: { screen: HomeContainer },
     ProfileContainer: { screen: ProfileContainer },
     PaymentsContainer: { screen: PaymentsContainer },
