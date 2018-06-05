0513d49ab9dc4eb68ee7bce97c5f67362585515a
Hyperspace018 <hyperspace018@gmail.com>
2018-06-05 04:12:32 -0400

    Create Edit Credit Card modal and Fix some navigation

--- app/containers/CreditCardContainer.js
+++ app/containers/CreditCardContainer.js
@@ -1,12 +1,47 @@
 import React, { Component } from 'react'
 import CreditCard from '../components/CreditCard'
+import CreditCards from '../particles/CreditCards'
+
+const dataCreditCards = [
+  {
+    cardNumber: '**** **** **** 0943',
+    validationDate: '17/11',
+    cvv: '123',
+    cardHolderName: 'Muhammad Isa Wijaya Kusuma'
+  },
+  {
+    cardNumber: '**** **** **** 8724',
+    validationDate: '03/12',
+    cvv: '123',
+    cardHolderName: 'Alfan Hibban Intiyas'
+  },
+]
 
 export default class CreditCardContainer extends Component{
+
+  state = {
+    modalVisibleEditCreditCard: false
+  }
+
+  toggleModalEditCreditCard(){
+    this.setState({modalVisibleEditCreditCard: !this.state.modalVisibleEditCreditCard})
+  }
 
   render(){
     return(
       <CreditCard
-        goback={() => this.props.navigation.goBack()}/>
+        goback={() => this.props.navigation.goBack()}
+        modalVisibleEditCreditCard={this.state.modalVisibleEditCreditCard}
+        toggleModalEditCreditCard={() => this.toggleModalEditCreditCard()}
+        
+        dataCreditCards={dataCreditCards}
+        renderCreditCards={({item}) => (
+          <CreditCards
+            cardNumber={item.cardNumber}
+            validationDate={item.validationDate}
+            action={() => this.toggleModalEditCreditCard()}/>
+        )}
+      />
     )
   }
 
