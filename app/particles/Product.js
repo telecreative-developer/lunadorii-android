715267a906c57b021d8d10c5c1ea5e88b8c0d824
Hyperspace018 <hyperspace018@gmail.com>
2018-06-07 04:08:28 -0400

    Fix button add to chart position

--- app/particles/Product.js
+++ app/particles/Product.js
@@ -7,15 +7,15 @@ import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityI
 const { width, height } = Dimensions.get('window')
 const Product = (props) => (
   <View style={styles.viewProduct}>
-    <ImageBackground source={{uri: props.image}} style={styles.ImageBackground}>
+    <ImageBackground source={{ uri: props.image }} style={styles.ImageBackground}>
       <TouchableOpacity style={styles.touchableOpacity}>
-        <Text style={styles.txtAddCart}><MaterialCommunityIcons name="cart"/> Add to Cart</Text>
+        <Text style={styles.txtAddCart}><MaterialCommunityIcons name="cart" /> Add to Cart</Text>
       </TouchableOpacity>
     </ImageBackground>
     <View style={styles.viewContent}>
       <Text style={styles.txtTitle}>{props.title}</Text>
       <Text style={styles.txtCategories}>{props.categories}</Text>
-      <View style={{marginTop: 10}}>
+      <View style={{ marginTop: 10 }}>
         <Rating
           type='custom'
           ratingCount={5}
@@ -30,50 +30,52 @@ const Product = (props) => (
   </View>
 )
 const styles = StyleSheet.create({
-  viewProduct:{
-    borderWidth: 1, 
-    borderColor: '#e2e2e2', 
-    marginRight: 10, 
+  viewProduct: {
+    borderWidth: 1,
+    borderColor: '#e2e2e2',
+    marginRight: 10,
     marginBottom: 10
   },
-  viewContent:{
+  viewContent: {
     padding: 10
   },
-  ImageBackground:{
-    width: ( width - 50 ) / 2, 
-    height: 100, 
+  ImageBackground: {
+    width: (width - 50) / 2,
+    height: 100,
     padding: 20
   },
-  txtCategories:{
-    fontSize: 14, 
+  txtCategories: {
+    fontSize: 14,
     maxWidth: width / 2.8
   },
-  txtTitle:{
+  txtTitle: {
     fontWeight: 'bold',
     fontSize: 16
   },
-  txtPrice:{
-    fontWeight: 'bold', 
-    fontSize: 16, 
+  txtPrice: {
+    fontWeight: 'bold',
+    fontSize: 16,
     maxWidth: width / 2.6
   },
-  txtAddCart:{
-    padding: 10, 
-    justifyContent: 'center', 
-    bottom: 5, 
-    color: '#000', 
+  txtAddCart: {
+    padding: 10,
+    justifyContent: 'center',
+    bottom: 5,
+    color: '#000',
     fontWeight: 'bold'
   },
-  txtPrice:{
-    fontWeight: 'bold', 
+  txtPrice: {
+    fontWeight: 'bold',
     alignSelf: 'flex-end'
   },
-  touchableOpacity:{
-    height: 30, 
-    backgroundColor: 'rgba(202, 202, 202, 0.73)', 
-    width: 120, 
-    left: 20, 
-    alignSelf: 'flex-end', 
+  touchableOpacity: {
+    height: 30,
+    backgroundColor: 'rgba(202, 202, 202, 0.73)',
+    marginRight: 5,
+    width: 100,
+    left: 20,
+    bottom: 15,
+    alignSelf: 'flex-end',
     borderRadius: 8
   }
 })
