f93f47c14b0bad2af571913e78c9c391ddb02d34
Hyperspace018 <hyperspace018@gmail.com>
2018-06-22 16:36:21 +0700

    Update Login Screen to latest design

--- app/containers/LoginContainer.js
+++ app/containers/LoginContainer.js
@@ -2,9 +2,20 @@ import React, { Component } from 'react'
 import Login from '../components/Login'
 
 export default class LoginContainer extends Component {
+
+  state={
+    passwordFieldVisibility: true,
+  }
+
+  togglePasswordFieldVisibility(){
+    this.setState({passwordFieldVisibility: !this.state.passwordFieldVisibility})
+  }
+
   render() {
     return (
-      <Login 
+      <Login
+        passwordFieldVisibility={this.state.passwordFieldVisibility}
+        togglePasswordFieldVisibility={() => this.togglePasswordFieldVisibility()}
         navigateToRegister={() => this.props.navigation.navigate("RegisterContainer")}/>
     )
   }
