69ff070b457a03d0d1136361bcebf5c8f1f2a474
Hyperspace018 <hyperspace018@gmail.com>
2018-06-22 14:31:41 +0700

    Create map for carousel

--- app/containers/HomeContainer.js
+++ app/containers/HomeContainer.js
@@ -185,7 +185,6 @@ class HomeContainer extends Component {
     await this.props.fetchBrandsProduct('123')
     await this.props.fetchProduct('123')
     await this.props.fetchBanners('123')
-    
   }
 
   render() {
@@ -193,6 +192,21 @@ class HomeContainer extends Component {
       <Home
         size={this.state.size}
        
+        dataBanners={[
+          {
+            image: 'https://i1.wp.com/3.bp.blogspot.com/-0z5ul05Ic6c/Vn6eCJJwuRI/AAAAAAAAMpY/PaTwKnYMsqI/s1600/produk%2Bkecantikan%2Bephyra%2Bskin%2Bbar.jpg?w=1140&ssl=1'
+          },
+          {
+            image: 'https://i1.wp.com/3.bp.blogspot.com/-0z5ul05Ic6c/Vn6eCJJwuRI/AAAAAAAAMpY/PaTwKnYMsqI/s1600/produk%2Bkecantikan%2Bephyra%2Bskin%2Bbar.jpg?w=1140&ssl=1'
+          },
+          {
+            image: 'https://i1.wp.com/3.bp.blogspot.com/-0z5ul05Ic6c/Vn6eCJJwuRI/AAAAAAAAMpY/PaTwKnYMsqI/s1600/produk%2Bkecantikan%2Bephyra%2Bskin%2Bbar.jpg?w=1140&ssl=1'
+          },
+          {
+            image: 'https://i1.wp.com/3.bp.blogspot.com/-0z5ul05Ic6c/Vn6eCJJwuRI/AAAAAAAAMpY/PaTwKnYMsqI/s1600/produk%2Bkecantikan%2Bephyra%2Bskin%2Bbar.jpg?w=1140&ssl=1'
+          }
+        ]}
+
         dataBrand={this.props.brandsproduct}
         renderBrand={({ item }) => (
           <Brand image={item.logo_url} />
@@ -211,7 +225,7 @@ class HomeContainer extends Component {
 
         dataProduct={this.props.product}
         renderProduct={({ item }) => (
-          <Product image={item.thumbnails[0].thumbnail_url} title={item.product} categories={item.subcategories.subcategory} price={item.price} star={item.product_rate}
+          <Product image={item.thumbnails[0].thumbnail_url} title={item.product} categories={item.subcategories.subcategory} price={item.price} star={item.product_rate} action={() => this.props.navigation.navigate("ProductShowContainer", { data: dataProduct[item.index] })}
           />
         )}
 
