dd5f93bead22763a84de4b5cd23370e9b1db9aab
ArifRachman <arifrachman@ArifRachmans-MacBook-Pro.local>
2018-06-22 18:07:11 +0700

    Updated endpoint categoryproduct

--- app/env/index.js
+++ app/env/index.js
@@ -1,2 +1,2 @@
-export const API_SERVER='http://2d723e8e.ngrok.io'
-export const API_BANNERS='http://f0ee6c12.ngrok.io'
+export const API_SERVER='http://f6b85836.ngrok.io'
+export const API_BANNERS='http://d82cfce2.ngrok.io'
