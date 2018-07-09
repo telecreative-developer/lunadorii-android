4ec75dc916c64d9c33421968bff884b2b8930582
rehanchrl <rehanchoirul@gmail.com>
2018-07-09 17:26:39 +0700

    change server

--- app/env/index.js
+++ app/env/index.js
@@ -1,4 +1,4 @@
-export const API_SERVER='http://ec2-18-222-130-109.us-east-2.compute.amazonaws.com/api/v1'
+export const API_SERVER='http://ec2-52-77-224-133.ap-southeast-1.compute.amazonaws.com/api/v1'
 // export const API_SERVER_AUTH    ="https://42619ba8.ngrok.io"
 // export const API_SERVER_USER    ="https://2cb61b70.ngrok.io"
 // export const API_SERVER_PRODUCT ="https://5a6efc17.ngrok.io"
