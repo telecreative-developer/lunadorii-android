fb6bdd02b9ab203601b957b54b422d5d843990de
Hyperspace018 <hyperspace018@gmail.com>
2018-07-17 16:28:45 +0700

    Commit today 9 validation on Category products

--- app/containers/RelatedToCategoryProductsContainer.js
+++ app/containers/RelatedToCategoryProductsContainer.js
@@ -60,9 +60,8 @@ class RelatedToCategoryProductsContainer extends Component{
 
   async componentDidMount(){
     const data = this.props.navigation.state.params.data
-    if(this.props.fetchProductWithCategory(data.product_subcategory_id)){
-        await this.setState({stillLoading: false})
-    }
+    await this.props.fetchProductWithCategory(data.product_subcategory_id)
+    await this.setState({stillLoading: false})
   }
 
   capitalize(string) {
@@ -86,22 +85,23 @@ class RelatedToCategoryProductsContainer extends Component{
         dataProduct={this.props.receiveProductWithCategory}
         renderProduct={({item}) => (
           <Product
-            image={item.thumbnails[0].thumbnail_url} 
-            title={item.title <= 17 ? this.capitalize(item.title) : this.capitalize(item.product).slice(0,18)+'...'} 
-            categories={item.brands[0].brand} 
+          image={item.thumbnails[0].thumbnail_url} 
+          title={item.title <= 17 ? this.capitalize(item.title) : this.capitalize(item.product).slice(0,18)+'...'} 
+          categories={item.brands[0].brand} 
             price={this.formatPrice(this.discountPrice(item.price, item.discount_percentage))} 
             star={item.product_rate} 
             action={() => this.props.navigation.navigate("ProductShowContainer", { data: item })}
             toggleModalAddToCart={() => this.toggleModalAddToCart(item)}
-          />
+            />
         )}
-
+        
         modalVisibleAddToCart={this.state.modalVisibleAddToCart}
         toggleModalAddToCart={() => this.toggleModalAddToCart()}
         onChangeQty={(qty) => this.setState({qty: parseInt(qty)})}
         handleAddToCart={() => this.handleAddToCart()}
-
+        
         image={this.props.navigation.state.params.data.thumbnail_url}
+        navigateToHome={() => this.props.navigation.navigate("HomeContainer")}
         goback={() => this.props.navigation.goBack()}
       />
     )
