9579628106c2033bb40555b32ff4b6af1e693716
alfanhib <alfanhib@gmail.com>
2018-07-17 11:45:46 +0700

    All Validasi

--- app/components/Wishlist.js
+++ app/components/Wishlist.js
@@ -17,41 +17,56 @@ const Wishlist = (props) => (
       backgroundColor="#f65857"
       barStyle="light-content"
     />
-    {props.stillLoading ? (
+    {/* {props.stillLoading ?
       <View style={styles.style}>
         <Spinner color="#d11e48"/>
       </View>
-    ) : (
+      :
       <Content>
-        {props.isEmpty ? (
-          <Validations title="Your Wishlist is empty" message1="Go shop to find what you need " message2="and add it to wishlist" navigateToProfile={props.navigateToProfile}/>
-        ) : (
-          props.stillLoading ? (
-            <View style={styles.viewArrivals}>
-              <View style={{backgroundColor: '#f6f6f6',height: 20, width: 100, marginBottom: 10}}/>
-              <FlatList
-                numColumns={2}
-                data={[1,2,3,4,5]}
-                renderItem={({item}) => (
-                  <View style={{width: (width - 35) / 2, height: 200, backgroundColor: '#f6f6f6',marginRight: 10,marginBottom: 10}}/>
-                )}
-              />
-            </View>
-          ) : (
-            <View style={styles.viewArrivals}>
-              <Text style={styles.yourWhisListTextTitle}>Your Wishlist</Text>
-              <FlatList
-                numColumns={2}
-                data={props.dataProduct}
-                renderItem={props.renderProduct}
-                keyExtractor={(item, index) => JSON.stringify(index)}
-                handleRemove={() => alert('hai')}
-              />
-            </View>
-          )
-        )}
+        {props.dataProduct.lenght == 0 ? 
+          <Validations 
+            title="Your Wishlist is empty" 
+            message1="Go shop to find what you need " 
+            message2="and add it to wishlist" 
+            navigateToProfile={props.navigateToProfile}/>
+          :
+          <View style={styles.viewArrivals}>
+            <Text style={styles.yourWhisListTextTitle}>Your Wishlist</Text>
+            <FlatList
+              numColumns={2}
+              data={props.dataProduct}
+              renderItem={props.renderProduct}
+              keyExtractor={(item, index) => JSON.stringify(index)}
+              handleRemove={() => alert('hai')}
+            />
+          </View>
+        }
       </Content>
-    )}
+    } */}
+    {props.stillLoading ? 
+      <View style={styles.style}>
+        <Spinner color="#d11e48"/>
+      </View>:
+      <Content>
+        {props.dataProduct.length == 0 ? 
+          <Validations 
+            title="Your Wishlist is empty" 
+            message1="Go shop to find what you need " 
+            message2="and add it to wishlist" 
+            navigateToProfile={props.navigateToProfile}/>:
+          <View style={styles.viewArrivals}>
+            <Text style={styles.yourWhisListTextTitle}>Your Wishlist</Text>
+            <FlatList
+              numColumns={2}
+              data={props.dataProduct}
+              renderItem={props.renderProduct}
+              keyExtractor={(item, index) => JSON.stringify(index)}
+              handleRemove={() => alert('hai')}
+            />
+          </View>
+        }
+      </Content>
+    }
   </Container>
 )
 
